e11416fc6ded3469c365137e87155e74a677dcc7
Tra Ngoc Nguyen <deadman.pro.qn@gmail.com>
2018-10-02 20:05:58 +0700

    add generator factory.

--- java-template/Generator/src/vgu/generator/GeneratorFactory.java
+++ java-template/Generator/src/vgu/generator/GeneratorFactory.java
@@ -12,30 +12,102 @@ import interfaces.AbstractComponent;
  * distribution and frequency of different generator types.
  *
  */
-public class GeneratorFactory {
-	public GeneratorFactory() {
+public class GeneratorFactory extends AbstractComponent {
+	private double power;
+
+	GeneratorFactory() {
 	}
 
+	GeneratorFactory(String name, double maxPower, double minPower, double maxChange, double minChange) {
+		this.name = name;
+		setMaxPower(maxPower);
+		setMinPower(minPower);
+		setMaxChange(maxChange);
+		setMinChange(minChange);
+		this.power = minPower;
+	}
+
+	public void next() {
+	};
+
+	public String getName() {
+		return name;
+	}
+
+	public double getPower() {
+		return power;
+	}
+
+	public void setPower(double power) {
+		if (this.power /* minPower */ == /* avgPower */power) {
+			return;
+		}
+
+		// absolute difference > maxChange
+		if (Math.abs(this.power/* minPower */ - /* avgPower */power) > getMaxChange()) {
+			this.power += getMaxChange();
+		}
+		// absolute difference < minChange
+		else if (Math.abs(this.power/* minPower */ - /* avgPower */power) < getMinChange()) {
+			this.power = (power + getMinChange());
+		}
+		// min <= abs diff <= max
+		else {
+			this.power = power;
+		}
+
+		// check the power after setting to keep it in the range [minPower, maxPower]
+		if (this.power > getMaxPower()) {
+			this.power = getMaxPower();
+		} else if (this.power < getMinPower()) {
+			this.power = getMinPower();
+		}
+	}
+
+	public double getCost() {
+		double coefficient = .5, maxPower = getMaxPower(), maxChange = getMaxChange(), minChange = getMinChange();
+
+		coefficient += maxPower < 2500 ? .25 : 0;
+		coefficient += maxChange > .5 * maxPower ? .25 : 0;
+		coefficient += minChange < .5 * maxPower ? .25 : 0;
+
+		return coefficient * getPower();
+	}
+
+	// generate a generator
 	public static AbstractComponent generate(String name, double maxPower, double minPower, double maxChange,
 			double minChange) {
-		GeneratorFactory f = new GeneratorFactory();
-		return f.new SingleGenerator(name, maxPower, minPower, maxChange, minChange);
-
+		return new GeneratorFactory(name, maxPower, minPower, maxChange, minChange);
 	}
 
-	/**
-	 * 
-	 * The nextGaussian() method returns random numbers with a mean of 0 and a
-	 * standard deviation of 1. to change the mean (average) of the distribution, we
-	 * add the required value; to change the standard deviation, we multiply the
-	 * value.
-	 * 
-	 * @param amount   Nr. of Values
-	 * @param avgPower Mean
-	 * @param std      Standard Deviation
-	 */
-	public static ArrayList<AbstractComponent> generate(int amount, double totalPower, double startPower) {
-		// TODO
-		return null;
+	// generate a list of generators
+	public static ArrayList<AbstractComponent> generate(int numGen, double totalPower, double initialPower) {
+		ArrayList<AbstractComponent> generator = new ArrayList();
+		double avgPower = totalPower / numGen;
+		for (int i = 0; i < numGen; ++i) {
+			generator.add(new GeneratorFactory("Generator_" + i, avgPower, 0.0, avgPower, avgPower));
+			if (initialPower > avgPower) {
+				// Generator g = (Generator) generator.get(i);
+				// System.out.println(g.getName());
+				generator.get(i).setPower(avgPower);
+				initialPower -= avgPower;
+			}
+		}
+		return generator;
 	}
+
+	// public void print() {
+	// ArrayList<AbstractComponent> al = generate(10, 500, 200);
+	// for (AbstractComponent c : al) {
+	// Generator g = (Generator) c;
+	// System.out.println(g.getName()+" "+c.getMaxPower());
+	// }
+	// }
+	//
+	// public static void main(String[] args) {
+	// Generator g1 = new Generator();
+	// g1.print();
+	// //Generator g2 = new Generator("G2", 2000, 100, 1, 1);
+	// //System.out.println(g2.getCost());
+	// }
 }
